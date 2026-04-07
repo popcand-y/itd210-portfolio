4162149b7bff8e37854dd8c525d5ede5871becbd
popcand-y <144555947+popcand-y@users.noreply.github.com>
2026-04-06 21:23:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,3 +57,19 @@ updateCharacter();
 }
 
 updateCharacter();
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const revealPoint = 120;
+
+    if (elementTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
